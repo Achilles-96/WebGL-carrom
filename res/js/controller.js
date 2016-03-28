ec3989d0fa737f739c56dce4541a8bd90e7dfb20
Achilles-96 <raghuram.vadapalli@research.iiit.ac.in>
2016-03-28 21:53:13 +0530

    Collision done

--- res/js/controller.js
+++ res/js/controller.js
@@ -18,7 +18,7 @@ var PWR_STATE = 2;
 var LAUNCH_STATE = 3;
 var state = STR_STATE;
 
-var speedx, speedy;
+var speedx=0, speedy=0, rcoinspeedx=0, rcoinspeedy=0;
 var bcoinspeedx = [0,0,0,0,0,0,0,0,0], bcoinspeedy = [0,0,0,0,0,0,0,0,0];
 var wcoinspeedx = [0,0,0,0,0,0,0,0,0], wcoinspeedy = [0,0,0,0,0,0,0,0,0];
 
@@ -85,106 +85,149 @@ function update() {
 			speedx = -speedx;
 		if(striker[0][1] + speedy >= 2.05 || striker[0][1] + speedy<= -2.05)
 			speedy = -speedy;
-		striker[0][0] += speedx;
-		striker[0][1] += speedy;
 		var i,j;
 		for(i=0;i<9;i++){
 			if(blackCoins[i][0] + bcoinspeedx[i] >= 2.05 || blackCoins[i][0] + bcoinspeedx[i] <= -2.05)
 				bcoinspeedx[i] = -bcoinspeedx[i];
 			if(blackCoins[i][1] + bcoinspeedy[i] >= 2.05 || blackCoins[i][1] + bcoinspeedy[i] <= -2.05)
 				bcoinspeedy[i] = -bcoinspeedy[i];
-			blackCoins[i][0] += bcoinspeedx[i];
-			blackCoins[i][1] += bcoinspeedy[i];
 			if(whiteCoins[i][0] + wcoinspeedx[i] >= 2.05 || whiteCoins[i][0] + wcoinspeedx[i] <= -2.05)
 				wcoinspeedx[i] = -wcoinspeedx[i];
 			if(whiteCoins[i][1] + wcoinspeedy[i] >= 2.05 || whiteCoins[i][1] + wcoinspeedy[i] <= -2.05)
 				wcoinspeedy[i] = -wcoinspeedy[i];
+			var x1 = striker[0][0]+speedx;
+			var y1 = striker[0][1]+speedy;
+			var x2 = blackCoins[i][0] + bcoinspeedx[i];
+			var y2 = blackCoins[i][1] + bcoinspeedy[i];
+			var dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			if(dist<0.23){
+				var speed_array = getNewSpeeds(x1,y1,x2,y2,speedx,speedy,bcoinspeedx[i],bcoinspeedy[i], 1.4, 1);
+				speedx = speed_array[0];
+				speedy = speed_array[1];
+				bcoinspeedx[i] = speed_array[2];
+				bcoinspeedy[i] = speed_array[3];
+			}
+			x2 = whiteCoins[i][0] + wcoinspeedx[i];
+			y2 = whiteCoins[i][1] + wcoinspeedy[i];
+			var dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			if(dist<0.23){
+				var speed_array = getNewSpeeds(x1,y1,x2,y2,speedx,speedy,wcoinspeedx[i],wcoinspeedy[i], 1.4, 1);
+				speedx = speed_array[0];
+				speedy = speed_array[1];
+				wcoinspeedx[i] = speed_array[2];
+				wcoinspeedy[i] = speed_array[3];
+			}
+			for(j=i+1;j<9;j++){
+				x1 = blackCoins[i][0] + bcoinspeedx[i];
+				y1 = blackCoins[i][1] + bcoinspeedy[i];
+				x2 = blackCoins[j][0] + bcoinspeedx[j];
+				y2 = blackCoins[j][1] + bcoinspeedy[j];
+				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+				if(dist<0.2){
+					var speed_array = getNewSpeeds(x1,y1,x2,y2,bcoinspeedx[i],bcoinspeedy[i],bcoinspeedx[j],bcoinspeedy[j], 1, 1);
+					bcoinspeedx[i] = speed_array[0];
+					bcoinspeedy[i] = speed_array[1];
+					bcoinspeedx[j] = speed_array[2];
+					bcoinspeedy[j] = speed_array[3];
+				}
+			}
+			for(j=0;j<9;j++){
+				x1 = blackCoins[i][0] + bcoinspeedx[i];
+				y1 = blackCoins[i][1] + bcoinspeedy[i];
+				x2 = whiteCoins[j][0] + wcoinspeedx[j];
+				y2 = whiteCoins[j][1] + wcoinspeedy[j];
+				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+				if(dist<0.2){
+					var speed_array = getNewSpeeds(x1,y1,x2,y2,bcoinspeedx[i],bcoinspeedy[i],wcoinspeedx[j],wcoinspeedy[j], 1, 1);
+					bcoinspeedx[i] = speed_array[0];
+					bcoinspeedy[i] = speed_array[1];
+					wcoinspeedx[j] = speed_array[2];
+					wcoinspeedy[j] = speed_array[3];
+				}
+			}
+			for(j=i+1;j<9;j++){
+				x1 = whiteCoins[i][0] + wcoinspeedx[i];
+				y1 = whiteCoins[i][1] + wcoinspeedy[i];
+				x2 = whiteCoins[j][0] + wcoinspeedx[j];
+				y2 = whiteCoins[j][1] + wcoinspeedy[j];
+				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+				if(dist<0.2){
+					var speed_array = getNewSpeeds(x1,y1,x2,y2,wcoinspeedx[i],wcoinspeedy[i],wcoinspeedx[j],wcoinspeedy[j], 1, 1);
+					wcoinspeedx[i] = speed_array[0];
+					wcoinspeedy[i] = speed_array[1];
+					wcoinspeedx[j] = speed_array[2];
+					wcoinspeedy[j] = speed_array[3];
+				}
+			}
+			x1 = redCoin[0][0] + rcoinspeedx;
+			y1 = redCoin[0][1] + rcoinspeedy;
+			x2 = blackCoins[i][0] + bcoinspeedx[i];
+			y2 = blackCoins[i][1] + bcoinspeedy[i];
+			dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			if(dist<0.2){
+				var speed_array = getNewSpeeds(x1,y1,x2,y2,rcoinspeedx,rcoinspeedy,bcoinspeedx[i],bcoinspeedy[i], 1, 1);
+				rcoinspeedx = speed_array[0];
+				rcoinspeedy = speed_array[1];
+				bcoinspeedx[i] = speed_array[2];
+				bcoinspeedy[i] = speed_array[3];
+			}
+			x2 = whiteCoins[i][0] + wcoinspeedx[i];
+			y2 = whiteCoins[i][1] + wcoinspeedy[i];
+			dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			if(dist<0.2){
+				var speed_array = getNewSpeeds(x1,y1,x2,y2,rcoinspeedx,rcoinspeedy,wcoinspeedx[i],wcoinspeedy[i], 1, 1);
+				rcoinspeedx = speed_array[0];
+				rcoinspeedy = speed_array[1];
+				wcoinspeedx[i] = speed_array[2];
+				wcoinspeedy[i] = speed_array[3];
+			}
+		}
+
+		for(i=0;i<9;i++){
+			bcoinspeedx[i]*=0.99;
+			bcoinspeedy[i]*=0.99;
+			wcoinspeedx[i]*=0.99;
+			wcoinspeedy[i]*=0.99;
+			blackCoins[i][0] += bcoinspeedx[i];
+			blackCoins[i][1] += bcoinspeedy[i];
 			whiteCoins[i][0] += wcoinspeedx[i];
 			whiteCoins[i][1] += wcoinspeedy[i];
 		}
-		for(i=0;i<9;i++){
-			var x1 = striker[0][0];
-			var y1 = striker[0][1];
-			var x2 = blackCoins[i][0];
-			var y2 = blackCoins[i][1];
-			var dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-			if(dist<0.25){
-				var angy = Math.PI/2.0 - Math.atan2(y2-y1,x2-x1);
-				var angx = Math.atan2(y2-y1,x2-x1);
-				bcoinspeedx[i]=Math.cos(angy)*Math.sin(angy)*speedy + Math.cos(angx)*Math.cos(angx)*speedx;
-				bcoinspeedy[i]=Math.cos(angy)*Math.cos(angy)*speedy + Math.cos(angx)*Math.sin(angx)*speedx;
-				speedx = -Math.cos(angy)*Math.sin(angy)*speedy - Math.cos(angx)*Math.cos(angx)*speedx;
-				speedy = -Math.cos(angy)*Math.cos(angy)*speedy - Math.cos(angx)*Math.sin(angx)*speedx;
-			}
-			for(j=i+1;j<9;j++){
-				x1 = blackCoins[j][0];
-				y1 = blackCoins[j][1];
-				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-				if(dist<0.2){
-					var angy = Math.PI/2.0 - Math.atan2(y2-y1,x2-x1);
-					var angx = Math.atan2(y2-y1,x2-x1);
-					bcoinspeedx[j] = Math.cos(angy)*Math.sin(angy)*bcoinspeedy[i] + Math.cos(angx)*Math.cos(angx)*bcoinspeedx[i];
-					bcoinspeedy[j] = Math.cos(angy)*Math.cos(angy)*bcoinspeedy[i] + Math.cos(angx)*Math.sin(angx)*bcoinspeedx[i];
-					bcoinspeedx[i] = -Math.cos(angy)*Math.sin(angy)*bcoinspeedy[i] - Math.cos(angx)*Math.cos(angx)*bcoinspeedx[i];
-					bcoinspeedy[i] = -Math.cos(angy)*Math.cos(angy)*bcoinspeedy[i] - Math.cos(angx)*Math.sin(angx)*bcoinspeedx[i];
-				}
-			}
-			for(j=0;j<9;j++){
-				x1 = whiteCoins[j][0];
-				y1 = whiteCoins[j][1];
-				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-				if(dist<0.23){
-					var angy = Math.PI/2.0 - Math.atan2(y2-y1,x2-x1);
-					var angx = Math.atan2(y2-y1,x2-x1);
-					wcoinspeedx[j] = Math.cos(angy)*Math.sin(angy)*bcoinspeedy[i] + Math.cos(angx)*Math.cos(angx)*bcoinspeedx[i];
-					wcoinspeedy[j] = Math.cos(angy)*Math.cos(angy)*bcoinspeedy[i] + Math.cos(angx)*Math.sin(angx)*bcoinspeedx[i];
-					bcoinspeedx[i] = -Math.cos(angy)*Math.sin(angy)*bcoinspeedy[i] - Math.cos(angx)*Math.cos(angx)*bcoinspeedx[i];
-					bcoinspeedy[i] = -Math.cos(angy)*Math.cos(angy)*bcoinspeedy[i] - Math.cos(angx)*Math.sin(angx)*bcoinspeedx[i];
-				}
-			}
-		}
-		for(i=0;i<9;i++){
-			var x1 = striker[0][0];
-			var y1 = striker[0][1];
-			var x2 = whiteCoins[i][0];
-			var y2 = whiteCoins[i][1];
-			var dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-			if(dist<0.25){
-				var angy = Math.PI/2.0 - Math.atan2(y2-y1,x2-x1);
-				var angx = Math.atan2(y2-y1,x2-x1);
-				wcoinspeedx[i]=Math.cos(angy)*Math.sin(angy)*speedy + Math.cos(angx)*Math.cos(angx)*speedx;
-				wcoinspeedy[i]=Math.cos(angy)*Math.cos(angy)*speedy + Math.cos(angx)*Math.sin(angx)*speedx;
-				speedx = -Math.cos(angy)*Math.sin(angy)*speedy - Math.cos(angx)*Math.cos(angx)*speedx;
-				speedy = -Math.cos(angy)*Math.cos(angy)*speedy - Math.cos(angx)*Math.sin(angx)*speedx;
-			}
-			for(j=i+1;j<9;j++){
-				x1 = whiteCoins[j][0];
-				y1 = whiteCoins[j][1];
-				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-				if(dist<0.23){
-					var angy = Math.PI/2.0 - Math.atan2(y2-y1,x2-x1);
-					var angx = Math.atan2(y2-y1,x2-x1);
-					wcoinspeedx[j] = Math.cos(angy)*Math.sin(angy)*wcoinspeedy[i] + Math.cos(angx)*Math.cos(angx)*wcoinspeedx[i];
-					wcoinspeedy[j] = Math.cos(angy)*Math.cos(angy)*wcoinspeedy[i] + Math.cos(angx)*Math.sin(angx)*wcoinspeedx[i];
-					wcoinspeedx[i] = -Math.cos(angy)*Math.sin(angy)*wcoinspeedy[i] - Math.cos(angx)*Math.cos(angx)*wcoinspeedx[i];
-					wcoinspeedy[i] = -Math.cos(angy)*Math.cos(angy)*wcoinspeedy[i] - Math.cos(angx)*Math.sin(angx)*wcoinspeedx[i];
-				}
-			}/*
-			for(j=0;j<9;j++){
-				x1 = blackCoins[j][0];
-				y1 = blackCoins[j][1];
-				dist = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-				if(dist<0.23){
-					var angy = Math.PI/2.0 - Math.atan2(y2-y1,x2-x1);
-					var angx = Math.atan2(y2-y1,x2-x1);
-					wcoinspeedx[j] = Math.cos(angy)*Math.sin(angy)*bcoinspeedy[i] + Math.cos(angx)*Math.cos(angx)*bcoinspeedx[i];
-					wcoinspeedy[j] = Math.cos(angy)*Math.cos(angy)*bcoinspeedy[i] + Math.cos(angx)*Math.sin(angx)*bcoinspeedx[i];
-					bcoinspeedx[i] = -Math.cos(angy)*Math.sin(angy)*bcoinspeedy[i] - Math.cos(angx)*Math.cos(angx)*bcoinspeedx[i];
-					bcoinspeedy[i] = -Math.cos(angy)*Math.cos(angy)*bcoinspeedy[i] - Math.cos(angx)*Math.sin(angx)*bcoinspeedx[i];
-				}
-			}*/
-		}
 		speedx = 0.99*speedx;
 		speedy = 0.99*speedy;
+		striker[0][0] += speedx;
+		striker[0][1] += speedy;
+
+		rcoinspeedx*=0.99;
+		rcoinspeedy*=0.99;
+		redCoin[0][0] += rcoinspeedx;
+		redCoin[0][1] += rcoinspeedy;
 	}
+}
+
+function getNewSpeeds(x1, y1, x2, y2, spx1, spy1, spx2, spy2, m1, m2){
+	var collisionision_angle = Math.atan2(y2 - y1, x2 - x1);
+
+	var speed1 = Math.sqrt(spx1*spx1 + spy1*spy1);
+	var speed2 = Math.sqrt(spx2*spx2 + spy2*spy2);
+
+	var direction_1 = Math.atan2(spy1, spx1);
+	var direction_2 = Math.atan2(spy2, spx2);
+	var new_xspeed_1 = speed1 * Math.cos(direction_1 - collisionision_angle);
+	var new_yspeed_1 = speed1 * Math.sin(direction_1 - collisionision_angle);
+	var new_xspeed_2 = speed2 * Math.cos(direction_2 - collisionision_angle);
+	var new_yspeed_2 = speed2 * Math.sin(direction_2 - collisionision_angle);
+
+	var final_xspeed_1 = ((m1 - m2) * new_xspeed_1 + (m2 + m2) * new_xspeed_2) / (m1 + m2);
+	var final_xspeed_2 = ((m1 + m1) * new_xspeed_1 + (m2 - m1) * new_xspeed_2) / (m1 + m2);
+	var final_yspeed_1 = new_yspeed_1;
+	var final_yspeed_2 = new_yspeed_2;
+
+	var cosAngle = Math.cos(collisionision_angle);
+	var sinAngle = Math.sin(collisionision_angle);
+	var vx1 = cosAngle * final_xspeed_1 - sinAngle * final_yspeed_1;
+	var vy1 = sinAngle * final_xspeed_1 + cosAngle * final_yspeed_1;
+	var vx2 = cosAngle * final_xspeed_2 - sinAngle * final_yspeed_2;
+	var vy2 = sinAngle * final_xspeed_2 + cosAngle * final_yspeed_2;
+	return [vx1, vy1, vx2, vy2];
 }
